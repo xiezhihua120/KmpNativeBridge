2c62a639408aafd5cc343fafd8addaca1f457df6
xiezhihua <xiezhihua@beemwork.com>
2023-11-21 18:20:29 +0800

    fix: gradlew use jbr-17

--- publishGithub.sh
+++ publishGithub.sh
@@ -1,3 +1,4 @@
+export JAVA_HOME="/Users/allin327/Library/Application Support/JetBrains/Toolbox/apps/AndroidStudio/ch-0/232.9559.62.2321.10773421/Android Studio Preview.app/Contents/jbr/Contents/Home"
 ./gradlew clean
 ./gradlew :nativeBridge-annotation:publish
 ./gradlew :nativeBridge-processor:publish
