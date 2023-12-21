b59a94fd6f2f8960791831b4c6b024d0b421a7df
xiezhihua <xiezhihua@beemwork.com>
2023-12-21 21:43:42 +0800

    feat: remove Java home set

--- publishGithub.sh
+++ publishGithub.sh
@@ -1,4 +1,3 @@
-export JAVA_HOME="/Users/allin327/Library/Application Support/JetBrains/Toolbox/apps/AndroidStudio/ch-0/232.9559.62.2321.10773421/Android Studio Preview.app/Contents/jbr/Contents/Home"
 ./gradlew clean
 ./gradlew :nativeBridge-annotation:publish
 ./gradlew :nativeBridge-processor:publish
